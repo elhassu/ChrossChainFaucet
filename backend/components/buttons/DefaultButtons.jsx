3573044200127961a725e126c3af865aa63ee7ee
Keelan Vella <keelan.x3@gmail.com>
2025-03-07 02:47:45 +0100

    Added default buttons (primary button)

---
+++ backend/components/buttons/DefaultButtons.jsx
@@ -0,0 +1,12 @@
+export function ButtonPrimary(props) {
+	const { className = "", children, as: Element = "button", ...rest } = props;
+
+	return (
+		<Element
+			className={`rounded-xl bg-web-orange-500 px-3 py-3 whitespace-nowrap font-bold ${className}`}
+			{...rest}
+		>
+			{children}
+		</Element>
+	);
+}
